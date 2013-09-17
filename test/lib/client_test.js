6dfb324ba24295d4913d3a3595352062e1f914eb
calleerlandsson <calle@omnidev.se>
2013-09-17 21:01:08 +0200

    added support for supplying username and password as environment variables

--- test/lib/client_test.js
+++ test/lib/client_test.js
@@ -14,4 +14,22 @@ describe('Client', function() {
     }).should.throwError(/password/);
     done();
   });
+
+  it('doesn\'t throw when username param isn\'t specified if the '+
+     'FORTYSIXELKS_USERNAME environment variable is defined', function(done) {
+    process.env.FORTYSIXELKS_USERNAME = 'user';
+    (function(){
+      new Client(undefined, 'pass');
+    }).should.not.throwError();
+    done();
+  });
+
+  it('doesn\'t throw when password param isn\'t specified if the '+
+     'FORTYSIXELKS_PASSWORD environment variable is defined', function(done) {
+    process.env.FORTYSIXELKS_PASSWORD = 'pass';
+    (function(){
+      new Client('user', undefined);
+    }).should.not.throwError();
+    done();
+  });
 });
